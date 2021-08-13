1a670a2d29ab17fd36f45be91bb27dafd125dcf6
Jorge Luis Perez <jolisper@pop-os.localdomain>
2021-08-13 13:59:03 -0300

    Bootloader is running in 32-bit protected mode
    - Add GDT config table
    - Add .load_protected rutine to jump to 32-bits mode.

--- boot.asm
+++ boot.asm
@@ -1,8 +1,11 @@
 ; Bootloader code is loaded (by BIOS) and running in memory at physical addresses 0x7C00 through 0x7DFF. 
 ; https://wiki.osdev.org/Memory_Map_(x86)
 
-[org 0x0]
+[org 0x7c00]
 [bits 16]
+
+CODE_SEGMENT equ gdt_code - gdt_start
+DATA_SEGMENT equ gdt_data - gdt_start
 
 bpb:
     ; The boot record contains both code and data, mixed together. The data that isn't code is known as the 
@@ -21,7 +24,7 @@ _start:
     ; CS, since there are many combinations of segment:offset that map to same physical 
     ; address. The far jump to 'start' forces a change in CS to 0x7c0. 
 
-    jmp 0x7c0:start
+    jmp 0x000:start
 
 start:
     ; Take control of segment registers is important because we don't know what the registers are going to be 
@@ -30,60 +33,67 @@ start:
     cli                 ; We disable interrupts here because executing an interrupt code may use segment
                         ; registers specially the stack.
 
-    mov ax, 0x7c0       ; Points data and extra segments to the beginning of the boot loader (0x7c00)
+    mov ax, 0x000       ; Points data and extra segments to the beginning of the boot loader (0x7c00)
     mov ds, ax
     mov es, ax
 
-    mov ax, 0x00        ; Stack grow downwards, so we set stack segment at 0x0000 but stack pointer to 0x7c00
-    mov ss, ax
+    mov ax, 0x00        
+    mov ss, ax          ; Stack grow downwards, so we set stack segment at 0x0000 but stack pointer to 0x7c00
     mov sp, 0x7c0
 
     sti                 ; Enables interrupts 
 
-    call load_buffer_from_disk
+.load_protected:
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
 
-    mov si, buffer
-    call print
+    jmp CODE_SEGMENT:load32
 
-   jmp $               ; Hangs
+; GDT
+; The Global Descriptor Table tells to the CPU about memory segments. We are using
+; just default values because we want to use memory paging, so, is not very important
+; the details of this configuration, but is mandatory to jump to 32-bits protected mode.
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+gdt_code:               ; CS should point here
+    dw 0xffff           ; Segment limit first 0-15 bits
+    dw 0                ; Base first 0-15 bits
+    db 0                ; Base 10-23 bits
+    db 0x9a             ; Access byte 
+    db 11001111b        ; High 4 bits flags and the low 4 bit flags
+    db 0                ; Base 24-31 bits
+gdt_data:               ; DS, SS, ES, FS, GS should point here
+    dw 0xffff           ; Segment limit first 0-15 bits
+    dw 0                ; Base first 0-15 bits
+    db 0                ; Base 10-23 bits
+    db 0x92             ; Access byte 
+    db 11001111b        ; High 4 bits flags and the low 4 bit flags
+    db 0                ; Base 24-31 bits
+gdt_end:
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
 
-load_buffer_from_disk:
-    mov ah, 2           ; read sector command
-    mov al, 1           ; one sector to read
-    mov ch, 0           ; cylinder low eight bits
-    mov cl, 2           ; read from sector 2
-    mov dh, 0           ; head number 0
-                        ; DL with disk number is already set for BIOS
-                        ; pointing to the same disk of the boot sector.
-    mov bx, buffer      ; ES:BX points to 
-    int 0x13
-    jc .error          ; print error message if any error
-    ret
-.error:
-    mov si, error_message
-    call print
-    ret
+[bits 32]
+load32:
+    mov ax, DATA_SEGMENT
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
 
-print:
-    mov bx, 0
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
+    ; Set the stack
+    mov ebp, 0x00200000
+    mov esp, ebp
 
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-error_message: db 'Failed to laod sector', 0
+    jmp $               ; Hangs
 
 ; Bootsector padding
 times 510-($-$$) db 0
 dw 0xaa55
-
-buffer:
