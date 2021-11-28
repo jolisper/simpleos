aab30fd817f158feccfc4c9a956d6e819e94f21c
Jorge Luis Perez <jolisper@pop-os.localdomain>
2021-11-28 16:49:27 -0300

    Add enable interrupts C function
    This new function is called after the IDT initialization in the kernel
    main function, which is a safer place to re-enable the system
    interruptions, because the IDT init is done. This call replace the old
    'cli' call in the kernel asm.

--- src/idt/idt.h
+++ src/idt/idt.h
@@ -18,4 +18,8 @@ struct idtr_desc {
 
 void idt_init();
 
+void enable_interrupts();
+
+void disable_interrupts();
+
 #endif
--- src/kernel.c
+++ src/kernel.c
@@ -72,4 +72,7 @@ void kernel_main() {
 
     // Initialize the interrupt descriptor table
     idt_init();
+
+    // Enable the system interrupts
+    enable_interrupts();
 }
